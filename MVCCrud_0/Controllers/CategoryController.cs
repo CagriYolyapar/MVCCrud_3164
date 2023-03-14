6dc0b173612b9bdf531e35e88b5c2ba21e642c6e
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-14 10:36:17 +0300

    Add project files.

---
+++ MVCCrud_0/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using MVCCrud_0.DesignPatterns.SingletonPattern;
+using MVCCrud_0.Models;
+using MVCCrud_0.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCrud_0.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindEntities _db;
+
+        public CategoryController()
+        {
+            _db = DBTool.DBInstance;
+        }
+        public ActionResult ListCategories()
+        {
+            List<CategoryVM> categories = _db.Categories.Select(x => new CategoryVM
+            {
+                ID = x.CategoryID,
+                CategoryName = x.CategoryName,
+                Description = x.Description
+            }).ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ MVCCrud_0/ViewModels/CategoryVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrud_0.ViewModels
+{
+    public class CategoryVM
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
